cb5d7e1340536efb3d828647b2b2cdb47891ed04
Riley Robinson <rrtoinfinity@gmail.com>
2021-08-28 11:00:09 -0400

    8-28-12021
    wiring up axios for api requests to the youtube data api

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAdSRguL5GW-SiK1y1e4eK3nCDSmqp4OKw' ;
+
+export default axios.create({
+
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
